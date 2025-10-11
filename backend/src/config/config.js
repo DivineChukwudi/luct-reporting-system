4dee32070a009131fc5cfb5761b08b46ce899643
Divine Chukwudi <chukwudidivine20@gmail.com>
2025-10-11 12:22:39 +0200

    Update config.js

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -6,7 +6,7 @@ export default {
         user: process.env.DB_USER || 'postgres',
         host: process.env.DB_HOST || 'localhost',
         database: process.env.DB_NAME || 'luct_reporting',
-        password: process.env.DB_PASSWORD || '0883',
+        password: process.env.DB_PASSWORD || 'postgres',
         port: process.env.DB_PORT || 5432,
         ssl: {
             rejectUnauthorized: false  // Required for Render
